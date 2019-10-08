4c7d8b76c8f8fd091392aa1bcbecede79e14c6fc
Xuan Hieu <xuanhieu1479@gmail.com>
2019-10-09 03:16:22 +0700

    Kiem tra co du email va mat khau ko

--- back_end/app/Http/Controllers/TaiKhoanController.php
+++ back_end/app/Http/Controllers/TaiKhoanController.php
@@ -11,6 +11,9 @@ class TaiKhoanController extends Controller
     public function taoMoi(Request $request) {
         $email = $request->input('email');
         $matkhau = $request->input('matkhau');
+        if (!$email || !$matkhau) return response()->json([
+            'message' => 'Yêu cầu đầy đủ email và mật khẩu',
+        ], 400, [], JSON_UNESCAPED_UNICODE);
         $taikhoan = new TaiKhoan();
         return $taikhoan->taoMoi($email, $matkhau);
     }
