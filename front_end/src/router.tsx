7509b4836956a1b9740a9a3d1d602d7c716914ac
Hieu Xuan Pham <Hieu.Xuan.Pham@external.mgm-tp.com>
2019-12-10 16:31:22 +0700

    Test Git Commit On Laptop.

--- front_end/src/router.tsx
+++ front_end/src/router.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from "react"; //Test Git Commit Laptop
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { HashRouter, Route, Switch } from "react-router-dom";
 import PageFront from "./pages/front"; 
